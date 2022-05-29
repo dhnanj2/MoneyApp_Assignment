628fe0f19a1e45336c4285f3f8655042355d2a03
Dhananjay <dhnanj2@gmail.com>
2022-05-29 16:29:18 +0530

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">IndiKart</a>        
+      </header>
+      <main> 
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
